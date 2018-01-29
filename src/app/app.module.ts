9ecdca7e365043db071a32c8c5e29b5fdca6840e
Crhistian Ramirez <cramirez@four51.com>
2018-01-29 14:52:33 -0600

    add ngx-toastr and use it to log errors to console
    extends angular ErrorHandler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // third party services
 import { CookieModule } from 'ngx-cookie';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OrderCloudModule } from '@ordercloud/angular-sdk';
-import { ToasterModule } from 'angular2-toaster';
+import { ToastrModule } from 'ngx-toastr';
 
 // app services
 import { OcAutoValidate } from './common/services/oc-auto-validate/oc-auto-validate.service';
@@ -17,6 +17,7 @@ import { OcAutoValidate } from './common/services/oc-auto-validate/oc-auto-valid
 // configs
 import { APP_CONFIG, OcAppConfig } from './config/app.config';
 import { OcSDKConfig } from './config/ordercloud-sdk.config';
+import { AppErrorHandler } from './config/error-handling.config';
 
 // app components
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './login/login-component';
 import { HomeComponent } from './home/home.component';
 import { ForgotPasswordFormComponent } from './login/forgot-password-form/forgot-password-form.component';
+
 
 @NgModule({
   declarations: [
@@ -50,11 +52,12 @@ import { ForgotPasswordFormComponent } from './login/forgot-password-form/forgot
     NgbModule.forRoot(),
     CookieModule.forRoot(),
     OrderCloudModule.forRoot(OcSDKConfig),
-    ToasterModule
+    ToastrModule.forRoot(),
   ],
   providers: [
     OcAutoValidate,
-    { provide: APP_CONFIG, useValue: OcAppConfig }
+    { provide: APP_CONFIG, useValue: OcAppConfig },
+    {provide: ErrorHandler, useClass: AppErrorHandler}
   ],
   bootstrap: [AppComponent]
 })
